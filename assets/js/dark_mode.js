bba51e6d1e419aeb15c04ce24cf9eeb2ba55c6ae
JD <Jdoublewoo@gmail.com>
2020-10-04 17:32:47 -0700

    Issue 105 : Dark Mode + Theming #118  (#119)
    * Dark Mode + Theming
    
    * Themes scss file created with default color scheme and dark color scheme.
    
    * Logic for managing dark mode toggle added.
    
    * Modified existing scss/html to work with theme toggling.
    
    * Fixed hover issues
    
    * Added enable_darkmode to configs.  
    
    * Added the requested option of disabling/enabling dark mode via config files.

---
+++ assets/js/dark_mode.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e44eadefce8273ed2478eed90fd46d058185fcc66e8e484a33c61b327771ade1
+size 959
---
+++ assets/js/theme.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c9eee028afa0ad57e9b88f541ab44b8e728ec4a4b2293d7e9473f03faa80af1d
+size 453
