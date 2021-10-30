c4ce6960736ff2fe7e42744363f17f35c48152e2
Maruan Al-Shedivat <maruan@genesistherapeutics.ai>
2021-10-30 19:53:39 -0400

    Remove theming dependence on jquery

--- assets/js/dark_mode.js
+++ assets/js/dark_mode.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2d441aa204aadfb19f761843d9ea43e892bdfcce4f44900251461bb97b309770
-size 223
+oid sha256:1c97de34ec54ea6a4c85a6a23c8dae1c5b05d763f9788c4f29a6aaa0896946ae
+size 253
