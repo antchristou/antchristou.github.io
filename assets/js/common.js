5a232a61877df21e5d336c440382f56d6ff52470
Rohan Deb Sarkar <rohandebsarkar@gmail.com>
2022-07-10 21:26:36 +0530

    Fix scripts (#755)

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:764120c310672d5c63541b9e84af7254b5a1c61f8d78ec9c218a54adaa68fe8c
-size 357
+oid sha256:3bcda6ab4c559d8e84cc04afb60424ad5b91f577f9aaa8697d23a7c9744cc5b1
+size 337
