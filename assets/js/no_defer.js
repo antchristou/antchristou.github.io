981bd770b65fa8faecd0c676c4d542709dc95b3b
George <31376482+george-gca@users.noreply.github.com>
2023-05-14 22:10:29 -0300

    Fix table color (#1424)
    Fix table color when dark mode is default.
    
    ---------
    
    Signed-off-by: George Araujo <george.gcac@gmail.com>

--- assets/js/no_defer.js
+++ assets/js/no_defer.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1f05b053beea1ac651bf9b08740c9955bd97ad4b0210b1107553e93abbc02735
-size 778
+oid sha256:d32997a7aa445eac3d4ae70f8aaec34a2dbe67198a939b7e395cabbf9fc92054
+size 768
