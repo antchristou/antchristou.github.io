308a7d124686357191fa975e5b051a7a2098d793
Samuel Chin <samuelchin91@gmail.com>
2023-06-10 21:30:13 -0400

    Fixes "General Information" section of CV in dark mode (#1432)
    When the dark mode is default, or if you set the page to dark mode and
    then refresh, the content in "General Information" can't be seen.
    This PR fixes the problem.

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ccd36f42133e1dbee46425137f6b1f26643461fc317ec5d1f2f2f6b4e6ce5074
-size 2264
+oid sha256:e0af8116c674252738339e0147e456cd33f646850e4b7a254158bbe8ddb2d0ac
+size 2273
