fc5443c2744f64bbb7ae19c37fd5fa1a6e97ef34
gzwongkk <gzwongkk@gmail.com>
2023-09-18 02:19:45 +0800

    Fix regex to limit keyword scope to fields (#1629)
    improved version of the original fix contributed by @paberr in #1429

--- _plugins/hideCustomBibtex.rb
+++ _plugins/hideCustomBibtex.rb
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f323228c1d7833d3f156ed5f4207d6fb47923bd44f02582655e0ee14b52d5b3c
-size 331
+oid sha256:064cc619ebd1b2570abc4fe034ffd3d2c7ba8228cdf5a987ab0c7b71e8c020b5
+size 342
