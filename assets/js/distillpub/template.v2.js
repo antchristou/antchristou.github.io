a4e1e1039c5d9e4b0b757d6dfb6ce6658cba4530
Yifan Jiang <55911052+YifanJiang233@users.noreply.github.com>
2023-10-08 17:15:01 +0100

    fix reference disappearing for distill blog in absence of author (#1804)

--- assets/js/distillpub/template.v2.js
+++ assets/js/distillpub/template.v2.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ac24800167f576873d51c47f885b32ebf085e2016e7c14d58728b825f3e2d0a
-size 292356
+oid sha256:8af3298bfe72593a79feb00e216c7cf4588553ef55ae67e27fdf1e49d4b3142f
+size 292359
